4b750c72379844b163e5c7fda3fc4fb7de7cb35a
Waqas Khan <wuikhan@gmail.com>
2018-02-09 13:09:37 -0500

    first commit

---
+++ dataFrame.py
@@ -0,0 +1,23 @@
+#activate virtual environment
+#source activate virtualEnvironment
+#install numpy - conda install numpy
+#install pandas - conda install pandas
+#run the program
+#python programname.py
+
+import numpy as np
+import pandas as pd
+
+data = {'Company':
+                        ['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+            'Person':
+                        ['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+            'Sales':
+                        [200,120,340,124,243,350]
+       }
+
+df = pd.DataFrame(data)
+print (df)
+
+byComp = df.groupby('Company')
+print (byComp.mean())
